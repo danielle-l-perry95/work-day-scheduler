e977a4d8a301c2d35ba933200f8836dd6d8540b5
Danielle Perry <nsbd95@gmail.com>
2022-11-16 15:18:19 -0500

    added moment function in js to console log current time

---
+++ script.js
@@ -0,0 +1,6 @@
+
+
+//Found on stack overflow, how to get current time in hours and minutes 
+moment(currentTime).format("hh:mm");
+var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
