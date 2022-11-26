b9b17f76fc345922c6f60923e8ce2ded1bf28399
Danielle Perry <nsbd95@gmail.com>
2022-11-26 11:56:30 -0500

    tried a different method for adding the classes

--- script.js
+++ script.js
@@ -5,12 +5,13 @@ var currentDay = moment()
 $('#currentDay').text(currentDay.format('dddd, MMMM Do YYYY, h:mm:ss a'))
 
 
-//Save input to local storage
+//Save button on click event 
 $('.saveBtn').click(function(e) {
     e.preventDefault()
     console.log($(this).siblings('input').val())
     var userText = $(this).siblings('input').val()
     var timeEl = $(this).siblings('span').attr('id')
+    //save to local storage 
     localStorage.setItem(timeEl, userText)
     // help with tutor on this part 
     // var output = JSON.parse(localStorage.getItem(timeEl, userText))
@@ -22,25 +23,52 @@ $('.saveBtn').click(function(e) {
 })
 
 
+
 //24 hour variable 
 moment(currentHour).format("HHmm");
 var currentHour = moment();
 
-    
 
-// display different classes based on the time
-// make an array for each hour? 
-var colorChange = function() {
-    var currentHour = moment().format('H')
-    var time = $(".hour")
-    time.forEach( () => {
-
-    })
-  
-    if (currentHour === time) {
+timeLoop.forEach (function () {
+    //return the integer of hour 
+    var hourId = parseInt($(this)('span').attr('id'))
+    if (currentHour === hourId) {
+        $(".hour").removeClass("past")
+        $(".hour").removeClass("future")
         $(".hour").addClass("present")
-    } else if (currentHour > time) {
+    } else if (currentHour > hourId) {
+        $(".hour").removeClass("future")
+        $(".hour").removeClass("present")
         $(".hour").addClass("past")
     } else
-        $("hour").addClass("future") 
-}   
+        $(".hour").removeClass("past")
+        $(".hour").removeClass("present")
+        $(".hour").addClass("future") 
+})
+timeLoop()
+
+
+
+
+// display different classes based on the time
+// var colorChange = function() {
+//     var currentHour = moment().format('H')
+//     var time = $(".hour")
+//     time.forEach( () => {
+//         if (currentHour === time) {
+//             $(".hour").removeClass("past")
+//             $(".hour").removeClass("future")
+//             $(".hour").addClass("present")
+//         } else if (currentHour > time) {
+//             $(".hour").removeClass("future")
+//             $(".hour").removeClass("present")
+//             $(".hour").addClass("past")
+//         } else
+//             $(".hour").removeClass("past")
+//             $(".hour").removeClass("present")
+//             $(".hour").addClass("future") 
+//     })
+//     }  
+    
+
+   
