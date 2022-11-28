d977745f1450e98d94b867ecb06589bfbad86007
Danielle Perry <nsbd95@gmail.com>
2022-11-28 18:32:16 -0500

    working on grabbing the id of the span elements to change the color based on time

--- script.js
+++ script.js
@@ -27,22 +27,28 @@ $('.saveBtn').click(function(e) {
 // 24 hour variable
 moment(currentHour).format("HHmm");
 var currentHour = moment();
+
 // Array of the hour id's 
-var hourElement = [
-    $('#09'),
-    $('#10'),
-    $('#11'),
-    $('#12'),
-    $('#13'),
-    $('#14'),
-    $('#15'),
-    $('#16'),
-    $('#17'),
-]
-// Loop to add the class based on time This does not work 
+// var hourElement = [
+//     $('#09'),
+//     $('#10'),
+//     $('#11'),
+//     $('#12'),
+//     $('#13'),
+//     $('#14'),
+//     $('#15'),
+//     $('#16'),
+//     $('#17'),
+// ]
+
+// var hourParse = parseInt(hourElement)
+// console.log(hourElement)
+// Loop to add the class based on time This does not work. Work on trying to grab each id beyond just the first one 
 function timeLoop() {
-    hourElement.forEach( () => {
-    if (currentHour === hourElement) {
+    $('.time-block').each( function() {
+        //loop through the id 
+       var hourElement = $(this).attr('id')
+    if (currentHour == hourElement) {
         $(".hour").removeClass("past")
         $(".hour").removeClass("future")
         $(".hour").addClass("present")
@@ -55,6 +61,7 @@ function timeLoop() {
         $(".hour").removeClass("present")
         $(".hour").addClass("future") 
 })} 
+timeLoop()
 
 
 
